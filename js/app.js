818d75e46ea781a1be90af25b1b0100b253d408f
SamBrownCoding <166549412+SamBrownCoding@users.noreply.github.com>
2024-08-29 11:19:46 -0500

    Cleaned update
    Clean up little bit of it to organized

--- js/app.js
+++ js/app.js
@@ -1,56 +1,60 @@
-//* KelvinToCelsius conversion:
+//#region Conversion Temp Function
+//! KelvinToCelsius conversion:
 //* kelvinNum - 273.15
 // Function to convert Kelvin to Celsius
 function kelvinToCelsius(kelvin) {
     return kelvin - 273.15;
-}
+};
 
-//* celsiusToKelvin:
+//! celsiusToKelvin:
 //* celsiusNum + 273.15
 // Function to convert Celsius to Kelvin
 function celsiusToKelvin(celsius) {
     return celsius + 273.15;
-}
+};
 
-//* fahrenheitToCelsius:
+//! fahrenheitToCelsius:
 //* (fahrenheit - 32) * 5/9
 // Function to convert Fahrenheit to Celsius
 function fahrenheitToCelsius(fahrenheit) {
     return (fahrenheit - 32) * 5 / 9;
-}
+};
 
-//* celsiusToFahrenheit:
+//! celsiusToFahrenheit:
 //* (celsius * 9/5) + 32
 // Function to convert Celsius to Fahrenheit
 function celsiusToFahrenheit(celsius) {
     return (celsius * 9 / 5) + 32;
-}
+};
 
-//* fahrenheitToKelvin:
+//! fahrenheitToKelvin:
 //* (fahrenheit - 32) * 5/9 + 273.15
 // Function to convert Fahrenheit to Kelvin
 function fahrenheitToKelvin(fahrenheit) {
     return (fahrenheit - 32) * 5 / 9 + 273.15;
-}
+};
 
-//* kelvinToFahrenheit: 
+//! kelvinToFahrenheit: 
 //* (kelvin - 273.15) * 9/5 + 32
 // Function to convert Kelvin to Fahrenheit
 function kelvinToFahrenheit(kelvin) {
     return (kelvin - 273.15) * 9 / 5 + 32;
-}
+};
+//#endregion
 
-
-
+//*Convert Button to show result!
 let submitBtn = document.getElementById("convertBtn");
+
+//* Grab the Answer for Result
 let result;
 
+//#region Convert Button AddEventListener
 submitBtn.addEventListener("click", () => {
+    //* Variable set up to get value.
     let inputTemp = document.getElementById("inputTemp").value;
-    //console.log(inputTemp);
     let fromUnit = document.getElementById("fromUnit").value;
     let toUnit = document.getElementById("toUnit").value;
-    //debugger
+
     //*Function to convert Celsius to Fahrenheit
     if (fromUnit === "Celsius" && toUnit === "Fahrenheit") {
         result = celsiusToFahrenheit(inputTemp);
@@ -75,18 +79,24 @@ submitBtn.addEventListener("click", () => {
         alert("                                     IT'S SAME TEMPERATURE!!")
     }
 
-   resultAnswer(result, fromUnit, toUnit);
+    resultAnswer(result, fromUnit, toUnit);
 
 });
+//#endregion
 
-function resultAnswer(result, fromUnit, toUnit){
+//#region Result Style Function
+//* Style the Result
+function resultAnswer(result, fromUnit, toUnit) {
     document.getElementById("result").innerText = `${fromUnit} to ${toUnit}\n ${parseInt(result).toFixed(2)}°`;
     document.getElementById("result").style.border = "solid 5px"
     document.getElementById("result").style.borderColor = "#203636";
     document.getElementById("result").style.width = "35%";
     document.getElementById("result").style.display = "inline-block";
-}
+};
+//#endregion
 
+//#region Reset Function
+//*Reset Button Function
 let ClearUp = document.getElementById("reset");
 
 ClearUp.addEventListener("click", () => {
@@ -95,8 +105,8 @@ ClearUp.addEventListener("click", () => {
     toUnit.value = "";
     document.getElementById("result").innerText = "";
     document.getElementById("result").style.border = "solid 0px"
-})
-
+});
+//#endregion
 
 
 
